01d0d1e6b30066db774dd2d2f028279fc4e97477
Jeremy Blum <jeremy@jeremyblum.com>
2015-12-23 17:30:52 -0800

    Working Time Pattern Generator Function

--- Software/WordClock.py
+++ Software/WordClock.py
@@ -6,6 +6,7 @@
 
 import sys, os, time, argparse
 from datetime import datetime
+from string import digits
 # Uses the rgbmatrix.so library that is imported via a git submodule.
 # Folder must be added to path to enable import
 sys.path.append(os.path.dirname(os.path.realpath(__file__)) + "/matrix")
@@ -58,7 +59,7 @@ m = {
     "two"       : {"row" : 9,  "start" : 4,  "length" : 3, "height" : 1},
     "three"     : {"row" : 9,  "start" : 7,  "length" : 5, "height" : 1},
     "eight"     : {"row" : 9,  "start" : 12, "length" : 5, "height" : 1},
-    "five"      : {"row" : 10, "start" : 1,  "length" : 4, "height" : 1},
+    "five2"     : {"row" : 10, "start" : 1,  "length" : 4, "height" : 1},
     "is2"       : {"row" : 10, "start" : 5,  "length" : 2, "height" : 1},
     "eleven"    : {"row" : 10, "start" : 7,  "length" : 6, "height" : 1},
     "nine"      : {"row" : 10, "start" : 13, "length" : 4, "height" : 1},
@@ -89,17 +90,26 @@ m = {
     "heart2"    : {"row" : 16, "start" : 16, "length" : 1, "height" : 1}
 }
 
-# Display an animation Sequences on the display
-def animation(seq="test"):
-    if seq == "test":
-        print "Running Test Sequence...",
+# Display an animation Sequence on the display
+def animation(seq="basic_test"):
+    if seq == "basic_test":
+        print "Testing each word & Color..."
         for key in m.iterkeys():
+            print "  %s" % (key)
             setDisplay([key], [255,0,0])
             time.sleep(0.2)
             setDisplay([key], [0,255,0])
             time.sleep(0.2)
             setDisplay([key], [0,0,255])
             time.sleep(0.2)
+        matrix.Clear()
+        print "Done."
+    elif seq == "time_test":
+        print "Testing All Times..."
+        for h in xrange(24):
+            for m in xrange(60):
+                setDisplay(getTimeWords(datetime(2016, 01, 01, h, m, 0)))
+                time.sleep(0.2)
         matrix.Clear()
         print "Done."
     else:
@@ -110,17 +120,97 @@ def getTimeWords(t = datetime.now()):
     words = []
 
     # If it's morning, we say "Good morning!"
-    # Otherwise, we just say "Hi" "Hiya"
-    if t.hour > 5 and t.hour < 10:
+    # Otherwise, we just say Hiya"
+    if t.hour > 5 and t.hour <= 10:
         words += ['good','morning!']
     else:
         words += ['hiya']
 
-    #"Carpe Diem", or "Time for Coffee"
+    # If it's Birthday, show that message. Otherwise...
+    # If it's early, it's "Time for Coffee"
+    # If it's a little later, we say "Carpe Diem"
     # If it's late, we say "Time for Sleep"
+    if t.month == 8 and t.day == 29:
+        words += ['happy','birthday']
+    elif t.hour > 5 and t.hour <= 9:
+        words += ['time','for','coffee']
+    elif t.hour > 9 and t.hour <= 12:
+        words += ['carpe','diem']
+    elif t.hour > 22 or t.hour < 3:
+        words += ['time','for','sleep']
 
-    #TODO: WIP
-    
+    # Minutes/OClock
+    words += ['it','is1']
+    if (t.minute <=2 and t.hour != 0 and t.hour !=12) or (t.minute > 57 and t.hour !=23 and t.hour !=11):
+        words += ['oclock']
+    elif t.minute > 2 and t.minute <= 7:
+        words += ['five1','minutes','past']
+    elif t.minute > 7 and t.minute <= 12:
+        words += ['ten1','minutes','past']
+    elif t.minute > 12 and t.minute <= 17:
+        words += ['a1','quarter','past']
+    elif t.minute > 17 and t.minute <= 22:
+        words += ['twenty','minutes','past']
+    elif t.minute > 22 and t.minute <= 27:
+        words += ['twenty','five1','minutes','past']
+    elif t.minute > 27 and t.minute <= 32:
+        words += ['half','past']
+    elif t.minute > 32 and t.minute <= 37:
+        words += ['twenty','five1','minutes','to']
+    elif t.minute > 37 and t.minute <= 42:
+        words += ['twenty','minutes','to']
+    elif t.minute > 42 and t.minute <= 47:
+        words += ['a1','quarter','to']
+    elif t.minute > 47 and t.minute <= 52:
+        words += ['ten1','minutes','to']
+    elif t.minute > 52 and t.minute <= 57:
+        words += ['five1','minutes','to']
+
+    #Hours
+    if t.minute > 32:
+        disp_hour = t.hour + 1
+    else:
+        disp_hour = t.hour
+
+    if disp_hour == 0 or disp_hour == 24:
+        words += ['midnight']
+    elif disp_hour == 12:
+        words += ['noon']
+    elif disp_hour == 1 or disp_hour == 13:
+        words += ['one']
+    elif disp_hour == 2 or disp_hour == 14:
+        words += ['two']
+    elif disp_hour == 3 or disp_hour == 15:
+        words += ['three']
+    elif disp_hour == 4 or disp_hour == 16:
+        words += ['four']
+    elif disp_hour == 5 or disp_hour == 17:
+        words += ['five2']
+    elif disp_hour == 6 or disp_hour == 18:
+        words += ['six']
+    elif disp_hour == 7 or disp_hour == 19:
+        words += ['seven']
+    elif disp_hour == 8 or disp_hour == 20:
+        words += ['eight']
+    elif disp_hour == 9 or disp_hour == 21:
+        words += ['nine']
+    elif disp_hour == 10 or disp_hour == 22:
+        words += ['ten2']
+    elif disp_hour == 11 or disp_hour == 23:
+        words += ['eleven']
+
+    #Time of Day
+    if (t.hour > 0 and t.hour < 11) or (t.hour == 0 and t.minute > 32) or (t.hour == 11 and t.minute <= 32):
+        words += ['in','the','morning']
+    elif (t.hour > 12 and t.hour < 18) or (t.hour == 12 and t.minute > 32):
+        words += ['in','the','afternoon']
+    elif (t.hour >= 18 and t.hour < 23) or (t.hour == 23 and t.minute <= 33):
+        words += ['in','the','evening']
+
+    print t.strftime('%I:%M%p'),
+    print "- " + (' '.join(words)).translate(None, digits)
+
+    return words
     
 
 
@@ -151,7 +241,7 @@ def setDisplay(words = [], color=[255,0,0], fade=0.1):
             frame_buff[i] = last_buff[i] + val*frame/19
 
         matrix.SetBuffer(frame_buff)
-        time.sleep(fade/20.0) 
+        time.sleep(fade/20.0/4.0) #Approximating this...
 
         #TDOD: Timing still seems wrong...
 
@@ -160,4 +250,4 @@ def setDisplay(words = [], color=[255,0,0], fade=0.1):
     
 
 #Main Execution
-animation("test")
+animation("time_test")
